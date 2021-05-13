85a6a1704205464a803ee2ebb04f149a5d5718e2
Helena Zubčeková <heli.zubcekova@gmail.com>
2021-05-13 12:32:02 +0200

    refactor DU

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 // tady je místo pro náš program
 
+let paragraph = document.getElementById("js-paragraph");
+let audio = document.getElementById("js-audio");
+
+let turnRed = document.getElementById("turnRed");
+let makeBigger = document.getElementById("makeBigger");
+let play = document.getElementById("play");
+let pause = document.getElementById("pause");
+let mute = document.getElementById("mute");
+let halfMute = document.getElementById("halfMute");
+let loud = document.getElementById("loud");
+let reset = document.getElementById("reset");
+
+turnRed.addEventListener("click", prepniZcervenani);
+makeBigger.addEventListener("click", zvetsitPismo);
+play.addEventListener("click", prehraj);
+pause.addEventListener("click", pauzni);
+mute.addEventListener("click", functionName=function(){upravHlasitost(0)});
+halfMute.addEventListener("click", functionName=function(){upravHlasitost(0.5)});
+loud.addEventListener("click", functionName=function(){upravHlasitost(1)});
+reset.addEventListener("click", resetuj);
+
 /**
  * Funkce přidá nebo odebere na prvku třídu, která se stará o červenou barvu písma
  * 
  * @param {string} elementId 
  */
-function prepniZcervenani(elementId) {
-    document.getElementById(elementId).classList.toggle('cerveny-text');
-}
+function prepniZcervenani() {
+    paragraph.classList.toggle('cerveny-text');
+};
 
 /**
  * Funkce zvětší/změnší písmo v elementu o definovanou velikost
@@ -15,25 +36,25 @@ function prepniZcervenani(elementId) {
  * @param {string} elementSelector 
  * @param {int} fontSizeChange 
  */
-function zvetsitPismo(elementSelector, fontSizeChange) {
-    let element = document.querySelector(elementSelector);
-    let currentFontSize = parseInt(element.style.fontSize);
-    element.style.fontSize = (currentFontSize + fontSizeChange) + 'px';
-}
+function zvetsitPismo() {
+    let fontSizeChange = 1;
+    let currentFontSize = parseInt(paragraph.style.fontSize);
+    paragraph.style.fontSize = (currentFontSize + fontSizeChange) + 'px';
+};
 
 /*--------------*/
 /* AUDIO FUNKCE */
 /*--------------*/
 
-function prehraj(elementSelector) {
-    document.querySelector(elementSelector).play();
+function prehraj() {
+    audio.play();
 }
-function pauzni(elementSelector) {
-    document.querySelector(elementSelector).pause();
+function pauzni() {
+    audio.pause();
 }
-function upravHlasitost(elementSelector, volumeValue) {
-    document.querySelector(elementSelector).volume = volumeValue;
+function upravHlasitost(volumeValue) {
+    audio.volume = volumeValue;
 }
-function resetuj(elementSelector) {
-    document.querySelector(elementSelector).currentTime = 0;
+function resetuj() {
+    audio.currentTime = 0;
 }
